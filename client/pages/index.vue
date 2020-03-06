eab1cc72568aecb3353c9924cbdf75cb82a0fe56
matt-muniz <mmunizsi01@gmail.com>
2020-03-06 18:26:03 -0500

    :skull: working on hero section

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -2,10 +2,47 @@
   <v-container>
     <!-- Hero section -->
     <div class="hero">
-      <v-row justify="center" align="center" style="height: 200px">
-        <div class="title text-center">
-          <img src="/title.svg" alt="" />
-        </div>
+      <v-row>
+        <v-col cols="12">
+          <v-row justify="center" align="center" style="height: 200px">
+            <div class="hero_title text-center">
+              <img src="/title.svg" alt="" />
+            </div>
+          </v-row>
+          <!-- Quote -->
+          <v-row justify="center" align="center">
+            <v-card
+              class="text-center my-md-10"
+              outlined
+              width="80%"
+              max-width="500"
+              color="#f8f8f8"
+            >
+              <div class="quote caption">
+                <p>
+                  "Play is often talked about as if it were a relief from
+                  serious learning.
+                </p>
+                <p>But for children play is serious learning.</p>
+                <p>Play is really the work of childhood."</p>
+                <p>-Fred Rodgers</p>
+              </div>
+            </v-card>
+          </v-row>
+          <!-- Status -->
+          <v-row justify="center" align="center" class="status">
+            <v-col cols="4" class="text-center">
+              <p class="title">
+                Need a babysitter? We have drop off babysitting services for
+                your convenience. Call today and let your child enjoy playing at
+                Serendipity's Playhouse and you can enjoy some child free time.
+                Call today 978-283-4335
+              </p>
+            </v-col>
+          </v-row>
+          <!-- Facebook cta -->
+          <v-row></v-row>
+        </v-col>
       </v-row>
     </div>
   </v-container>
@@ -18,12 +55,20 @@ export default {
 </script>
 
 <style scoped>
-.title {
-  width: 400px;
+p {
+  margin: 0;
+  padding: 0;
 }
-@media (max-width: 750px) and (orientation: portrait) and (max-width: 1000px) and (orientation: landscape) {
-  .title {
-    width: 300px;
+.hero_title {
+  width: 300px;
+}
+.quote {
+  color: #808080;
+  border: 1px solid silver;
+}
+@media (min-width: 750px) {
+  .hero_title {
+    width: 400px;
   }
 }
 </style>
