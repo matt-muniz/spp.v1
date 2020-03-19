592a1471a5520c338369095e36c1bf96ee5c7bf0
matt <mmunizsi01@gmail.com>
2020-03-19 15:25:41 -0400

    :sparkles: figured out my checkbox issue :sparkles:

--- client/pages/bookings/index.vue
+++ client/pages/bookings/index.vue
@@ -22,64 +22,91 @@
           <v-col cols="12" sm="6">
             <v-checkbox
               v-model="checkbox"
-              value="1"
-              :label="`Checkbox 1: ${checkbox.toString()}`"
+              value="oneCharacter"
+              :label="`Visit from 1 character: ${checkbox.toString()}`"
+              :disabled="disableOneCharCheckbox"
               @change="showCharOne"
             ></v-checkbox>
-            <div v-if="hidden1">
+            <div v-if="showTimeCharOne">
               <v-checkbox
                 v-model="checkbox"
-                value="50"
-                :label="`15 min: ${checkbox.toString()}`"
+                :value="v1"
+                :disabled="disable15MinCheckbox"
+                :label="`15 minute rental: ${checkbox.toString()}`"
+                @change="disableCheckbox"
               ></v-checkbox>
               <v-checkbox
                 v-model="checkbox"
-                value="100"
-                :label="`30 min: ${checkbox.toString()}`"
+                :value="v2"
+                :disabled="disable30MinCheckbox"
+                :label="`30 minute rental: ${checkbox.toString()}`"
+                @change="disableCheckbox"
               ></v-checkbox>
             </div>
             <v-checkbox
               v-model="checkbox"
               value="2"
-              :label="`Checkbox 1: ${checkbox.toString()}`"
+              :disabled="disableTwoCharCheckbox"
+              :label="`Visit from 2 characters: ${checkbox.toString()}`"
               @change="showBothCharSelect"
             ></v-checkbox>
+            <div v-if="showTimeCharTwo">
+              <v-checkbox
+                v-model="checkbox"
+                :value="v1"
+                :disabled="disable15MinCheckbox"
+                :label="`15 minute rental: ${checkbox.toString()}`"
+                @change="disableCheckbox"
+              ></v-checkbox>
+              <v-checkbox
+                v-model="checkbox"
+                :value="v2"
+                :disabled="disable30MinCheckbox"
+                :label="`30 minute rental: ${checkbox.toString()}`"
+                @change="disableCheckbox"
+              ></v-checkbox>
+            </div>
             <v-checkbox
               v-model="checkbox"
-              value="80"
-              :label="`15 min: ${checkbox.toString()}`"
-            ></v-checkbox>
-            <v-checkbox
-              v-model="checkbox"
-              value="160"
-              :label="`30 min: ${checkbox.toString()}`"
-            ></v-checkbox>
-            <v-checkbox
-              v-model="checkbox"
-              value="3"
-              :label="`Checkbox 1: ${checkbox.toString()}`"
+              value="oneHour"
+              :label="`Add 1 hour: ${checkbox.toString()}`"
             ></v-checkbox>
           </v-col>
           <v-col cols="12" sm="6">
             <v-checkbox
               v-model="checkbox"
-              value="4"
-              :label="`Checkbox 1: ${checkbox.toString()}`"
+              value="fullSheet"
+              :label="`Full sheet of cake: ${checkbox.toString()}`"
             ></v-checkbox
             ><v-checkbox
               v-model="checkbox"
-              value="5"
-              :label="`Checkbox 1: ${checkbox.toString()}`"
+              value="vanilla"
+              :label="`Vanilla cake: ${checkbox.toString()}`"
             ></v-checkbox
             ><v-checkbox
               v-model="checkbox"
-              value="6"
-              :label="`Checkbox 1: ${checkbox.toString()}`"
+              value="chocolate"
+              :label="`Chocolate cake: ${checkbox.toString()}`"
+            ></v-checkbox>
+            <v-checkbox
+              v-model="checkbox"
+              value="chocolate"
+              :label="`Chocolate cake: ${checkbox.toString()}`"
+            ></v-checkbox>
+            <v-checkbox
+              v-model="checkbox"
+              value="chocolate"
+              :label="`Chocolate cake: ${checkbox.toString()}`"
+            ></v-checkbox>
+            <v-checkbox
+              v-model="checkbox"
+              value="chocolate"
+              :label="`Chocolate cake: ${checkbox.toString()}`"
             ></v-checkbox>
           </v-col>
         </v-row>
 
-        <div v-if="hidden1">
+        <div v-if="showOneCharSelect">
           <v-select
             :items="items"
             label="Select your character - 1"
@@ -88,7 +115,7 @@
             background-color="#C8ECFF"
           ></v-select>
         </div>
-        <div v-if="hidden2">
+        <div v-if="showTwoCharSelect">
           <v-select
             :items="items"
             label="Select your character - 2"
@@ -124,22 +151,60 @@ export default {
     valid: true,
     checkbox: [],
     items: ['Foo', 'Bar', 'Fizz', 'Buzz'],
-    hidden1: false,
-    hidden2: false
+    showOneCharSelect: false,
+    showTwoCharSelect: false,
+    showTimeCharOne: false,
+    showTimeCharTwo: false,
+    v1: '',
+    v2: '',
+    disableTwoCharCheckbox: false,
+    disableOneCharCheckbox: false,
+    disable15MinCheckbox: false,
+    disable30MinCheckbox: false
   }),
+  computed: {},
   methods: {
     showCharOne(val) {
-      console.log(val.includes())
+      this.showOneCharSelect = !this.showOneCharSelect
+      this.showTimeCharOne = !this.showTimeCharOne
 
-      if (val.includes() !== -1) {
-        console.log('checked')
-      } else {
-        console.log('unchecked')
-      }
+      this.filterValues('50', '100')
+      this.disableTwoCharCheckbox = !this.disableTwoCharCheckbox
     },
     showBothCharSelect() {
-      this.hidden1 = !this.hidden1
-      this.hidden2 = !this.hidden2
+      this.showTwoCharSelect = !this.showTwoCharSelect
+      this.showOneCharSelect = !this.showOneCharSelect
+      this.showTimeCharTwo = !this.showTimeCharTwo
+
+      this.filterValues('80', '160')
+      this.disableOneCharCheckbox = !this.disableOneCharCheckbox
+    },
+    filterValues(a, b) {
+      this.showOneCharSelect ? (this.v1 = a) : (this.v1 = '')
+      this.showOneCharSelect ? (this.v2 = b) : (this.v1 = '')
+      const removeV1V2 = this.checkbox.filter(
+        (item) => item !== a && item !== b
+      )
+      this.disable15MinCheckbox = false
+      this.disable30MinCheckbox = false
+      this.checkbox = removeV1V2
+    },
+    disableCheckbox() {
+      const disable30Min = this.checkbox.filter((item) => {
+        item === '50' || item === '80'
+          ? (this.disable30MinCheckbox = true)
+          : (this.disable30MinCheckbox = false)
+      })
+
+      const disable15Min = this.checkbox.filter((item) =>
+        item === '100' || item === '160'
+          ? (this.disable15MinCheckbox = true)
+          : (this.disable15MinCheckbox = false)
+      )
+      return {
+        disable30Min,
+        disable15Min
+      }
     }
   }
 }
